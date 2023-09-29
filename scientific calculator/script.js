290d7773cd86d04a9eb7c6a62ba85ecb5ab951a5
Abdul-cm <84390908+Abdul-cm@users.noreply.github.com>
2023-09-29 10:45:32 +0100

    Add files via upload

---
+++ scientific calculator/script.js
@@ -0,0 +1,17 @@
+function insert(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function backspace() {
+    var result = document.getElementById('result').value;
+    document.getElementById('result').value = result.substring(0, result.length - 1);
+}
+
+function calculate() {
+    var result = document.getElementById('result').value;
+    document.getElementById('result').value = eval(result);
+}
